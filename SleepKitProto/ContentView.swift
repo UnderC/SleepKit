28e8c8e7b64a152a5d0703c626cd1d7190114a9f
Lee Hyun <ink0416@naver.com>
2020-06-26 23:10:35 +0900

    Initial Commit

---
+++ SleepKitProto/ContentView.swift
@@ -0,0 +1,21 @@
+`//
+//  ContentView.swift
+//  SleepKitProto
+//
+//  Created by 현 on 2020/06/26.
+//  Copyright © 2020 현. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
